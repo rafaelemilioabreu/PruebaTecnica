65050d37db014f92b15635a6959cc73a2642b444
rafael <rafaelemilioabreuvaldez@gmail.com>
2021-07-29 17:33:20 -0400

    Solicitud controlador agregado

---
+++ PruebaTecnica/Controllers/SolicitudController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PruebaTecnica.Data;
+using PruebaTecnica.Models;
+
+namespace PruebaTecnica.Controllers
+{
+    public class SolicitudController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SolicitudController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Solicitud
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Solicitud.ToListAsync());
+        }
+
+        // GET: Solicitud/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var solicitud = await _context.Solicitud
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (solicitud == null)
+            {
+                return NotFound();
+            }
+
+            return View(solicitud);
+        }
+
+        // GET: Solicitud/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Solicitud/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Estado,FechaCreacion")] Solicitud solicitud)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(solicitud);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(solicitud);
+        }
+
+        // GET: Solicitud/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var solicitud = await _context.Solicitud.FindAsync(id);
+            if (solicitud == null)
+            {
+                return NotFound();
+            }
+            return View(solicitud);
+        }
+
+        // POST: Solicitud/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Estado,FechaCreacion")] Solicitud solicitud)
+        {
+            if (id != solicitud.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(solicitud);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SolicitudExists(solicitud.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(solicitud);
+        }
+
+        // GET: Solicitud/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var solicitud = await _context.Solicitud
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (solicitud == null)
+            {
+                return NotFound();
+            }
+
+            return View(solicitud);
+        }
+
+        // POST: Solicitud/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var solicitud = await _context.Solicitud.FindAsync(id);
+            _context.Solicitud.Remove(solicitud);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SolicitudExists(int id)
+        {
+            return _context.Solicitud.Any(e => e.Id == id);
+        }
+    }
+}
