a83cfbe48a58c218931836fc4ba681f922e6d935
rafael <rafaelemilioabreuvaldez@gmail.com>
2021-07-29 15:59:44 -0400

    modelos y primer controlador agregado

---
+++ PruebaTecnica/Models/Personas.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica.Models
+{
+    public class Personas
+    {
+     
+
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre Obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Apellido Obligatorio")]
+
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "Fecha de nacimiento Obligatoria")]
+
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "Pasaporte Obligatorio")]
+
+        public string Pasaporte { get; set; }
+
+        [Required(ErrorMessage = "Direccion Obligatorio")]
+
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "Sexo Obligatorio")]
+
+        public string Sexo { get; set; }
+    }
+}
---
+++ PruebaTecnica/Models/Solicitud.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica.Models
+{
+    public class Solicitud
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "La persona id es Obligatorio")]
+        [ForeignKey("Personas")]
+        public Personas PersonaId { get; set; }
+
+        [Required(ErrorMessage = "Estado Obligatorio")]
+
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "Fecha de creacion Obligatoria")]
+
+        public DateTime FechaCreacion { get; set; }
+
+     
+
+      
+
+    }
+}
