a90ec37a431ab1fdae9c86d47dbc2820dfee4d5b
rafael <rafaelemilioabreuvaldez@gmail.com>
2021-07-30 13:42:12 -0400

    algoritmo 2 agregado

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
             Funcion();
 
             void Funcion()
